cf5fc465b3526e57ed83b402773db307ccc9e18d
root <root@ip20.ip-51-89-168.eu>
2024-09-30 10:50:46 -0400

    logs changed

--- __main__.py
+++ __main__.py
@@ -197,6 +197,7 @@ def logs(id: str):
 @app.put("/meta5/password/change/loginID/{id}/", tags=["meta5"])
 async def change_meta_password(id:int, chpwd: ChangePassword):
     login, old, new, broker = (id, chpwd.old, chpwd.new, chpwd.broker)
+    logger.debug(f"{chpwd = }")
     try:
         response = await change_meta5_account_password(login, old, new, broker)
         logger.debug(f"{response = }")
